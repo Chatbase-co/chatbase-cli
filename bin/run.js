f825036789d6798e27237276660f23b0d5b5b184
Aly ElNaggar <aly.elnaggar@chatbase.co>
2026-08-10 11:53:32 +0300

    fix: use oclif additionalHelpFlags instead of argv-munging for -h support

--- bin/run.js
+++ bin/run.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
 import { execute } from '@oclif/core'
 
-// Convert -h to --help for consistency with common CLI conventions
-const argv = process.argv.map((arg) => (arg === '-h' ? '--help' : arg))
-process.argv = argv
-
 await execute({ dir: import.meta.url })
